2f2e5b61d52bb710d4c853ac8b65f56d2e475856
MohammadH98 <mohammad.1011@hotmail.com>
2021-03-14 21:20:23 -0400

    front end for home screen

--- WatchNext-Frontend/app/screens/HomeScreen.js
+++ WatchNext-Frontend/app/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import { TextInput, Text, Title, Searchbar, Caption, IconButton, Colors } from "react-native-paper";
+import { TextInput, Text, Title, Searchbar, Caption, IconButton } from "react-native-paper";
 
 export default class HomeScreen extends Component {
     constructor(props)
@@ -22,82 +22,84 @@ export default class HomeScreen extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
+            <KeyboardAvoidingView style={styles.container} enabled={false}>
                 <View style={styles.top}>
-                    {/*<LinearGradient
-                        colors={['purple', 'fuchsia']}
-                        style={styles.linearGradient}
-                        start={{x: 0, y: 0.5}}
-                        end={{x: 1, y: 0.5}}
+                    <LinearGradient
+                            colors={['purple', 'fuchsia']}
+                            style={styles.linearGradient}
+                            start={{x: 0, y: 0.5}}
+                            end={{x: 1, y: 0.5}}
                     >
                         <IconButton
                             icon="qrcode-scan"
-                            color={Colors.white}
-                            size={20}
+                            color='white'
+                            size={35}
                             onPress={() => console.log('QR Code Icon Pressed')}
                             style={{flex: 1}}
                         />
                         <Searchbar 
-                            placeholder="Add Friends by Username..." 
+                            placeholder="Add Friends..." 
                             onChangeText={(text) => this.onChangeSearch(text)} 
                             value={this.state.searchQuery}
-                            style={{flex: 5}}
+                            style={{flex: 6}}
                         />
-                    </LinearGradient>*/}
-                    <IconButton
-                        icon="qrcode-scan"
-                        color={Colors.white}
-                        size={20}
-                        onPress={() => console.log('QR Code Icon Pressed')}
-                        style={{flex: 1}}
-                    />
-                    <Searchbar 
-                        placeholder="Add Friends by Username..." 
-                        onChangeText={(text) => this.onChangeSearch(text)} 
-                        value={this.state.searchQuery}
-                        style={{flex: 5}}
-                    />
-                </View>  
+                        <IconButton
+                            icon="account-plus"
+                            color='white'
+                            size={35}
+                            onPress={() => console.log('Add Friend Icon Pressed')}
+                            style={{flex: 1}}
+                        />
+                    </LinearGradient>
+                </View>
                 <View style={styles.content}>
-                    <Title>Matching Sessions</Title>
                 </View>
                 <View style={styles.bottom}>
-                    <Title>Bottom Bar</Title>
+                    <IconButton
+                        icon="home"
+                        color='purple'
+                        size={40}
+                        onPress={() => console.log('Home Icon Pressed')}
+                        style={{flex: 1}}
+                    />
+                    <IconButton
+                        icon="account"
+                        color='black'
+                        size={40}
+                        onPress={() => console.log('User Icon Pressed')}
+                        style={{flex: 1}}
+                    />
                 </View>
-            </View>
+            </KeyboardAvoidingView>
         );
     }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  linearGradient: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 5,
-    height: 100
+    flex: 1
   },
   top: 
   {
     flex: 1,
-    backgroundColor: 'purple',
-    alignItems: "center",
-    justifyContent: "center",
-    flexDirection: "row"
   },
   content: 
   {
-    flex: 4,
-    backgroundColor: 'white'
+    flex: 4.5,
+    backgroundColor: 'blue'
   },
   bottom: 
   {
-    flex: 1,
-    backgroundColor: 'purple'
+    flex: 0.5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  linearGradient: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    height: 200,
+    width: '100%',
   }
 });
