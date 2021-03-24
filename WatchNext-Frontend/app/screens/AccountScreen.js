811983e8a65a6e4ff2f839f8c2e258e1f54ff491
MohammadH98 <mohammad.1011@hotmail.com>
2021-03-24 01:42:00 -0400

    reformatted code

--- WatchNext-Frontend/app/screens/AccountScreen.js
+++ WatchNext-Frontend/app/screens/AccountScreen.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-  View,
-  KeyboardAvoidingView,
-  Platform,
-} from "react-native";
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 import {
   Text,
   Title,
@@ -19,34 +14,41 @@ import {
   TextInput,
 } from "react-native-paper";
 
-function ExportData() 
-{
-    return [
-      "Adventure",
-      "Horror",
-      "Action",
-      "Sci-Fi",
-      "Comedy",
-      "Drama",
-      "Thriller",
-      "Crime",
-      "Fantasy",
-      "Mystery",
-      "Romance",
-      "Anime",
-      "Documentary",
-      "For Kids",
-      "Bollywood",
-      "Independent",
-      "Musical",
-      "Political",
-    ];
+function ExportData() {
+  return [
+    "Adventure",
+    "Horror",
+    "Action",
+    "Sci-Fi",
+    "Comedy",
+    "Drama",
+    "Thriller",
+    "Crime",
+    "Fantasy",
+    "Mystery",
+    "Romance",
+    "Anime",
+    "Documentary",
+    "For Kids",
+    "Bollywood",
+    "Independent",
+    "Musical",
+    "Political",
+  ];
 }
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = { genres: ExportData(), selectedGenres: [], genrePage: true, name: "Bilal Jaffry", username: "BJ_1998", editName: false, editUsername: false };
+    this.state = {
+      genres: ExportData(),
+      selectedGenres: [],
+      genrePage: true,
+      name: "Bilal Jaffry",
+      username: "BJ_1998",
+      editName: false,
+      editUsername: false,
+    };
   }
 
   setName(name) {
@@ -57,18 +59,15 @@ export default class HomeScreen extends Component {
     this.setState({ username: username });
   }
 
-  editName() 
-  {
+  editName() {
     this.setState({ editName: !this.state.editName });
   }
 
-  editUsername() 
-  {
+  editUsername() {
     this.setState({ editUsername: !this.state.editUsername });
   }
 
-  toggleGenre(genre) 
-  {
+  toggleGenre(genre) {
     var newChecked = this.state.selectedGenres;
 
     if (newChecked.includes(genre)) {
@@ -81,14 +80,12 @@ export default class HomeScreen extends Component {
     this.setState({ selectedGenres: newChecked });
   }
 
-  toggleGenrePage()
-  {
-      this.setState({genrePage: true});
+  toggleGenrePage() {
+    this.setState({ genrePage: true });
   }
 
-  toggleSettingsPage()
-  {
-      this.setState({genrePage: false});
+  toggleSettingsPage() {
+    this.setState({ genrePage: false });
   }
 
   render() {
@@ -115,171 +112,202 @@ export default class HomeScreen extends Component {
         </View>
         <View style={styles.details}>
           <View>
-            <Avatar.Image size={150} source={{uri: "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/44680002_954885824711081_5944810765792837632_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=qpcUDcGVykcAX-fJ5Vl&_nc_ht=scontent-yyz1-1.xx&oh=1a3183b173ec701a8b12b4e318d35dcb&oe=607E1E31"}} />
-            <FAB 
+            <Avatar.Image
+              size={150}
+              source={{
+                uri:
+                  "https://scontent-yyz1-1.xx.fbcdn.net/v/t1.0-9/44680002_954885824711081_5944810765792837632_n.jpg?_nc_cat=111&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=qpcUDcGVykcAX-fJ5Vl&_nc_ht=scontent-yyz1-1.xx&oh=1a3183b173ec701a8b12b4e318d35dcb&oe=607E1E31",
+              }}
+            />
+            <FAB
               icon="camera"
-              style={{backgroundColor: "purple", position: "absolute", top: 50, left: 120}}
+              style={{
+                backgroundColor: "purple",
+                position: "absolute",
+                top: 50,
+                left: 120,
+              }}
               color="white"
               onPress={() => console.log("Avatar Change Button Pressed")}
             />
           </View>
-          <Headline style={{fontWeight: "bold"}}>{this.state.name}</Headline>
+          <Headline style={{ fontWeight: "bold" }}>{this.state.name}</Headline>
           <Subheading>{this.state.username}</Subheading>
         </View>
         <View style={styles.edit}>
           <View style={styles.buttons}>
-              <Button
-                  mode="text"
-                  labelStyle={{color: this.state.genrePage ? "purple" : "black"}}
-                  onPress={() => this.toggleGenrePage()}
-              >
-                  Genres
-              </Button>
-              <Button
-                  mode="text"
-                  labelStyle={{color: this.state.genrePage ? "black" : "purple"}}
-                  onPress={() => this.toggleSettingsPage()}
-              >
-                  Settings
-              </Button>
+            <Button
+              mode="text"
+              labelStyle={{ color: this.state.genrePage ? "purple" : "black" }}
+              onPress={() => this.toggleGenrePage()}
+            >
+              Genres
+            </Button>
+            <Button
+              mode="text"
+              labelStyle={{ color: this.state.genrePage ? "black" : "purple" }}
+              onPress={() => this.toggleSettingsPage()}
+            >
+              Settings
+            </Button>
           </View>
-          <Divider/>
-          {this.state.genrePage ? 
-            (
-              <View>
-                <Caption style={{marginTop: 15, marginBottom: 10, alignSelf: "center", fontSize: 16}}>Favourite Genres</Caption>
-                <View style={styles.genres}>
-                  {this.state.genres.map((genre) => (
-                    <Button
-                      key={genre}
-                      mode="contained"
-                      compact="true"
-                      style=
-                      {{
-                        width: "30%",
-                        backgroundColor: this.state.selectedGenres.includes(genre)
-                          ? "purple"
-                          : "lightgrey",
-                        margin: 3,
-                        marginBottom: 15, 
-                        borderRadius: 20          
-                      }}
-                      labelStyle=
-                      {{
-                        color: this.state.selectedGenres.includes(genre)
-                          ? "white"
-                          : "black",
-                        fontSize: 11.5  
-                      }}
-                      onPress={() => this.toggleGenre(genre)}
-                    >
-                      {genre}
-                    </Button>
-                  ))}
-                </View>
+          <Divider />
+          {this.state.genrePage ? (
+            <View>
+              <Caption
+                style={{
+                  marginTop: 15,
+                  marginBottom: 10,
+                  alignSelf: "center",
+                  fontSize: 16,
+                }}
+              >
+                Favourite Genres
+              </Caption>
+              <View style={styles.genres}>
+                {this.state.genres.map((genre) => (
+                  <Button
+                    key={genre}
+                    mode="contained"
+                    compact="true"
+                    style={{
+                      width: "30%",
+                      backgroundColor: this.state.selectedGenres.includes(genre)
+                        ? "purple"
+                        : "lightgrey",
+                      margin: 3,
+                      marginBottom: 15,
+                      borderRadius: 20,
+                    }}
+                    labelStyle={{
+                      color: this.state.selectedGenres.includes(genre)
+                        ? "white"
+                        : "black",
+                      fontSize: 11.5,
+                    }}
+                    onPress={() => this.toggleGenre(genre)}
+                  >
+                    {genre}
+                  </Button>
+                ))}
               </View>
-            ) :
-            (
+            </View>
+          ) : (
+            <View>
               <View>
-                <View>
-                  <View style={styles.editContainer}>
-                  {!this.state.editName && <Caption style={{fontSize: 14}}>Name</Caption>}
-                    <View style={styles.editFields}>
-                      {!this.state.editName ? 
-                        <Text style={{fontSize: 16}}>{this.state.name}</Text> : 
-                        <TextInput
-                          label="Name"
-                          
-                          placeholder={this.state.name}
-                          value={this.state.name}
-                          onChangeText={(text) => this.setName(text)}
-                          style={{ width: "75%"}}
-                        />}  
-                      <Button
-                        mode="text"
-                        style={{ marginLeft: "auto", marginRight: 10}}
-                        labelStyle={{ color: "purple" }}
-                        onPress={() => this.editName()}
-                      >
-                        {!this.state.editName ? "Edit" : "Save"}
-                      </Button>
-                    </View>
+                <View style={styles.editContainer}>
+                  {!this.state.editName && (
+                    <Caption style={{ fontSize: 14 }}>Name</Caption>
+                  )}
+                  <View style={styles.editFields}>
+                    {!this.state.editName ? (
+                      <Text style={{ fontSize: 16 }}>{this.state.name}</Text>
+                    ) : (
+                      <TextInput
+                        label="Name"
+                        placeholder={this.state.name}
+                        value={this.state.name}
+                        onChangeText={(text) => this.setName(text)}
+                        style={{ width: "75%" }}
+                      />
+                    )}
+                    <Button
+                      mode="text"
+                      style={{ marginLeft: "auto", marginRight: 10 }}
+                      labelStyle={{ color: "purple" }}
+                      onPress={() => this.editName()}
+                    >
+                      {!this.state.editName ? "Edit" : "Save"}
+                    </Button>
                   </View>
-                  <View style={styles.editContainer}>
-                  {!this.state.editUsername && <Caption style={{fontSize: 14}}>Username</Caption>}
-                    <View style={styles.editFields}>
-                      {!this.state.editUsername ? 
-                      <Text style={{fontSize: 16}}>{this.state.username}</Text> : 
+                </View>
+                <View style={styles.editContainer}>
+                  {!this.state.editUsername && (
+                    <Caption style={{ fontSize: 14 }}>Username</Caption>
+                  )}
+                  <View style={styles.editFields}>
+                    {!this.state.editUsername ? (
+                      <Text style={{ fontSize: 16 }}>
+                        {this.state.username}
+                      </Text>
+                    ) : (
                       <TextInput
                         label="Username"
-                        
                         placeholder={this.state.username}
                         value={this.state.username}
                         onChangeText={(text) => this.setUsername(text)}
-                        style={{ width: "75%"}}
-                      />}   
-                      <Button
-                        mode="text"
-                        style={{ marginLeft: "auto", marginRight: 10 }}
-                        labelStyle={{ color: "purple" }}
-                        onPress={() => this.editUsername()}
-                      >
-                        {!this.state.editUsername ? "Edit" : "Save"}
-                      </Button>
-                    </View>
-                  </View>
-                  <View style={styles.editContainer}>
-                    <Caption style={{fontSize: 14}}>Email</Caption>
-                    <View style={styles.editFields}>
-                      <Text style={{fontSize: 16}}>bj1998@mcmaster.ca</Text>
-                      <Button
-                        mode="text"
-                        style={{ marginLeft: "auto", opacity: 0 }}
-                        labelStyle={{ color: "purple" }}
-                        onPress={() => console.log("Edit Email Pressed")}
-                      >
-                        Edit
-                      </Button>
-                    </View>
+                        style={{ width: "75%" }}
+                      />
+                    )}
+                    <Button
+                      mode="text"
+                      style={{ marginLeft: "auto", marginRight: 10 }}
+                      labelStyle={{ color: "purple" }}
+                      onPress={() => this.editUsername()}
+                    >
+                      {!this.state.editUsername ? "Edit" : "Save"}
+                    </Button>
                   </View>
                 </View>
-                <View style={{alignSelf: 'center'}}> 
-                  <Button
-                    mode="contained"
-                    icon = "lock"
-                    style={{ backgroundColor: "purple", borderRadius: 20, marginTop: this.state.editUsername ? 75 : 53 }}
-                    onPress={() => console.log("Edit Password Pressed")}
-                  >
-                    Change Password
-                  </Button>
-                  <Button
-                    mode="contained"
-                    icon = "delete"
-                    style={{ backgroundColor: "red", borderRadius: 20, marginTop: 10 }}
-                    onPress={() => console.log("Delete Account Pressed")}
-                  >
-                    Delete Account
-                  </Button>
+                <View style={styles.editContainer}>
+                  <Caption style={{ fontSize: 14 }}>Email</Caption>
+                  <View style={styles.editFields}>
+                    <Text style={{ fontSize: 16 }}>bj1998@mcmaster.ca</Text>
+                    <Button
+                      mode="text"
+                      style={{ marginLeft: "auto", opacity: 0 }}
+                      labelStyle={{ color: "purple" }}
+                      onPress={() => console.log("Edit Email Pressed")}
+                    >
+                      Edit
+                    </Button>
+                  </View>
                 </View>
               </View>
-            )
-          }          
-        </View>     
+              <View style={{ alignSelf: "center" }}>
+                <Button
+                  mode="contained"
+                  icon="lock"
+                  style={{
+                    backgroundColor: "purple",
+                    borderRadius: 20,
+                    marginTop: this.state.editUsername ? 75 : 53,
+                  }}
+                  onPress={() => console.log("Edit Password Pressed")}
+                >
+                  Change Password
+                </Button>
+                <Button
+                  mode="contained"
+                  icon="delete"
+                  style={{
+                    backgroundColor: "red",
+                    borderRadius: 20,
+                    marginTop: 10,
+                  }}
+                  onPress={() => console.log("Delete Account Pressed")}
+                >
+                  Delete Account
+                </Button>
+              </View>
+            </View>
+          )}
+        </View>
         <View styles={styles.bottomContainer}>
           <Divider />
           <View style={styles.bottom}>
-              <IconButton
-                  icon="home"
-                  color="black"
-                  size={40}
-                  onPress={() => console.log("Home Icon Pressed")}
-              />
-              <IconButton
-                  icon="account"
-                  color="purple"
-                  size={40}
-                  onPress={() => console.log("User Icon Pressed")}
-              />
+            <IconButton
+              icon="home"
+              color="black"
+              size={40}
+              onPress={() => console.log("Home Icon Pressed")}
+            />
+            <IconButton
+              icon="account"
+              color="purple"
+              size={40}
+              onPress={() => console.log("User Icon Pressed")}
+            />
           </View>
         </View>
       </KeyboardAvoidingView>
@@ -288,58 +316,48 @@ export default class HomeScreen extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: 
-  {
+  container: {
     flex: 1,
   },
-  top: 
-  {
+  top: {
     flex: 1,
     alignItems: "center",
     justifyContent: "space-between",
     flexDirection: "row",
   },
-  details: 
-  {
+  details: {
     flex: 1.5,
     alignItems: "center",
-    justifyContent: "flex-end"
+    justifyContent: "flex-end",
   },
-  edit: 
-  {
+  edit: {
     flex: 3.5,
     justifyContent: "center",
   },
-  buttons: 
-  {
+  buttons: {
     justifyContent: "space-around",
     flexDirection: "row",
   },
-  genres: 
-  {
+  genres: {
     alignItems: "center",
     justifyContent: "center",
     flexDirection: "row",
     flexWrap: "wrap",
   },
-  editContainer:
-  {
-    marginLeft: 15, 
-    marginTop: 10
+  editContainer: {
+    marginLeft: 15,
+    marginTop: 10,
   },
-  editFields: 
-  {
+  editFields: {
     flexDirection: "row",
     alignItems: "center",
   },
-  bottomContainer:
-  {
+  bottomContainer: {
     flex: 1,
   },
-  bottom: 
-  {
+  bottom: {
     alignItems: "center",
     justifyContent: "space-around",
     flexDirection: "row",
-  }
+  },
 });
