6a9d5ffd2933c4da18a443b9d7746ffc2dfd4387
MohammadH98 <mohammad.1011@hotmail.com>
2021-04-23 09:40:07 -0400

    centered loading screen

--- WatchNext-Frontend/app/screens/LoadingScreen.js
+++ WatchNext-Frontend/app/screens/LoadingScreen.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import { ActivityIndicator, Text } from "react-native-paper";
+import { ActivityIndicator, Headline } from "react-native-paper";
 import { View } from "react-native";
 
 export default class LoadingScreen extends Component {
   render() {
     return (
-      <View>
-        <ActivityIndicator animating={true} color={"red"} />
-        <Text>Now Loading...</Text>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size={40} animating={true} color={"#6200ee"} />
+        <Headline style={{marginTop: 25}}>Now Loading...</Headline>
       </View>
     );
   }
